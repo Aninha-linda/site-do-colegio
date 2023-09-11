b3baf0b5e7d1f171d2f6ecb2cd57eda4d5be6b4b
Ana Carolina <129754161+Aninha-linda@users.noreply.github.com>
2023-09-11 08:42:32 -0300

    k

--- main.js
+++ main.js
@@ -2,5 +2,12 @@ function TocaSomPom () {
 document.querySelector('#som-teclado-pom').play();
 }
 
-const ListaDeTeclas = document.querySelectorAll('.tecla')
+const ListaDeTeclas = document.querySelectorAll('.tecla');
 
+let contador = 0;
+
+while(contador < 9){
+    ListaDeTeclas[0].onclick = TocaSomPom;
+contador = contador + 1;
+console.log(contador);
+}
