42dc6a67f92d202ae82c9610f07e9d354a09fbb5
Ana Carolina <129754161+Aninha-linda@users.noreply.github.com>
2023-09-25 07:55:00 -0300

    j

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ while(contador < ListaDeTeclas.length) {
     const classe = ListaDeTeclas[Contador].classList[1];
     console.log(classe);
     const idAudio = 'som_${classe}';
+    console.log(idAudio);
     Contador = Contador + 1;
 console.log(contador);
 }
