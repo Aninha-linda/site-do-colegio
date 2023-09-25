23b2c1fd3aa5b5742c4c5c7073c6f49213c9e9ae
Ana Carolina <129754161+Aninha-linda@users.noreply.github.com>
2023-09-25 07:50:58 -0300

    g

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
-function TocaSomPom () {
-document.querySelector('#som-teclado-pom').play();
+function tocar(idElementoAudio) {
+document.querySelector(idElementoAudio).play();
 }
 
 const ListaDeTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0;
 
-while(contador < 9){
-    ListaDeTeclas[0].onclick = TocaSomPom;
-contador = contador + 1;
+while(contador < ListaDeTeclas.length) {
+    ListaDeTeclas[Contador].onclick = function(){
+        tocar('#som_tecla-toim');
+    };
+    const classe = ListaDeTeclas[Contador].classList[1];
+    console.log(classe);
+    const idAudio = 'som_${classe}';
+    Contador = Contador + 1;
 console.log(contador);
 }
