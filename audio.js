d731501d8371615f14a7844b2b9bbd567aa2f135
Ana Carolina <129754161+Aninha-linda@users.noreply.github.com>
2023-11-13 08:08:11 -0300

    1

---
+++ audio.js
@@ -0,0 +1,21 @@
+function tocar(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+const ListaTeclas = document.querySelectorAll('.tecla');
+
+for (let Contador = 0; Contador < ListaTeclas.length; Contador++) {
+    const botão = ListaTeclas[Contador];
+    const tecla = botão.classList[1];
+    const idAudio = `#som_${tecla}`;
+    botão.onclick = function () {
+        tocar(idAudio);
+    }
+    botão.onkeydown = function (Evento) {
+        if (Evento.code === 'Space' || Evento.code === 'Enter') {
+            botão.classList.add('ativa');
+        }
+    }
+    botão.onkeyup = function () {
+        botão.classList.remove('ativa');
+    }
+}
